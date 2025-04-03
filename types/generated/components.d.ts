2cf4cc46f42628b47c298179b9cc47a0fc0a40d6
l8onrodney <info@leightonrodney.com>
2025-04-03 18:03:16 -0400

    Added Page content type, and cta component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,29 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface CtaCta extends Struct.ComponentSchema {
+  collectionName: 'components_cta_ctas';
+  info: {
+    displayName: 'cta';
+    icon: 'volumeUp';
+  };
+  attributes: {
+    ctaBgColor: Schema.Attribute.String & Schema.Attribute.Required;
+    ctaBtnBgColor: Schema.Attribute.String & Schema.Attribute.Required;
+    ctaBtnText: Schema.Attribute.String & Schema.Attribute.Required;
+    ctaBtnUrl: Schema.Attribute.String & Schema.Attribute.Required;
+    ctaHeading: Schema.Attribute.String & Schema.Attribute.Required;
+    ctaImage: Schema.Attribute.Media<'images'>;
+    ctaIsExternal: Schema.Attribute.Boolean &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<false>;
+    ctaSubHeading: Schema.Attribute.Text;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'cta.cta': CtaCta;
+    }
+  }
+}
